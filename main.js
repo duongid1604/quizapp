110df2b9c4f69bcb418da2bca2f7fba5ac132fb1
duongid1604 <91748123+duongid1604@users.noreply.github.com>
2022-07-22 10:27:49 +0700

    Update main.js

--- main.js
+++ main.js
@@ -1,11 +1,18 @@
-function buildQuiz() {
+function buildQuiz(){
+  // variable to store the HTML output
   const output = [];
 
+  // for each question...
   myQuestions.forEach(
     (currentQuestion, questionNumber) => {
+
+      // variable to store the list of possible answers
       const answers = [];
 
-      for (letter in currentQuestion.answers) {
+      // and for each available answer...
+      for(letter in currentQuestion.answers){
+
+        // ...add an HTML radio button
         answers.push(
           `<label>
             <input type="radio" name="question${questionNumber}" value="${letter}">
@@ -14,12 +21,16 @@ function buildQuiz() {
           </label>`
         );
       }
-      output.push (
+
+      // add this question and its answers to the output
+      output.push(
         `<div class="question"> ${currentQuestion.question} </div>
         <div class="answers"> ${answers.join('')} </div>`
       );
     }
   );
+
+  // finally combine our output list into one string of HTML and put it on the page
   quizContainer.innerHTML = output.join('');
 }
 
